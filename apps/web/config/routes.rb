e959b13c12edb060da94a92b64f70656ae2b48a4
Kohei Yamamoto <kymmt90@gmail.com>
2018-08-15 21:42:08 +0900

    hanami generate action web calendars#update

--- apps/web/config/routes.rb
+++ apps/web/config/routes.rb
@@ -1,3 +1,4 @@
 root to: 'calendars#index'
 
 resources :calendars, only: [:index, :show, :new, :create, :edit]
+patch '/calendars/:id', to: 'calendars#update'
---
+++ apps/web/views/calendars/update.rb
@@ -0,0 +1,5 @@
+module Web::Views::Calendars
+  class Update
+    include Web::View
+  end
+end
