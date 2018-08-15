3162adf99f5dbe5529d23eac6f0ad4ded1d5f5e6
Kohei Yamamoto <kymmt90@gmail.com>
2018-08-15 15:48:34 +0900

    Add calendars#index action

--- apps/web/controllers/calendars/index.rb
+++ apps/web/controllers/calendars/index.rb
@@ -2,7 +2,10 @@ module Web::Controllers::Calendars
   class Index
     include Web::Action
 
+    expose :calendars
+
     def call(params)
+      @calendars = CalendarRepository.new.all
     end
   end
 end
--- spec/web/controllers/calendars/index_spec.rb
+++ spec/web/controllers/calendars/index_spec.rb
@@ -2,8 +2,24 @@ RSpec.describe Web::Controllers::Calendars::Index, type: :action do
   let(:action) { described_class.new }
   let(:params) { Hash[] }
 
-  it 'is successful' do
-    response = action.call(params)
-    expect(response[0]).to eq 200
+  context 'when there are no calendars' do
+    it 'is successful' do
+      response = action.call(params)
+
+      expect(response[0]).to eq 200
+    end
+  end
+
+  context 'when there are calendars' do
+    it 'is successful' do
+      repository = CalendarRepository.new
+      calendar = repository.find(GccgFactory[:calendar].id)
+      another_calendar = repository.find(GccgFactory[:calendar].id)
+
+      response = action.call(params)
+
+      expect(response[0]).to eq 200
+      expect(action.calendars).to match_array [calendar, another_calendar]
+    end
   end
 end
