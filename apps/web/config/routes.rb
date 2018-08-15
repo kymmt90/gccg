18f4cd01fa6d9fa89ced2e3e834867bc34ad80ae
Kohei Yamamoto <kymmt90@gmail.com>
2018-08-15 19:22:55 +0900

    hanami generate action web calendars#create

--- apps/web/config/routes.rb
+++ apps/web/config/routes.rb
@@ -1,3 +1,4 @@
 root to: 'calendars#index'
 
 resources :calendars, only: [:index, :show, :new]
+post '/calendars', to: 'calendars#create'
---
+++ apps/web/controllers/calendars/create.rb
@@ -0,0 +1,8 @@
+module Web::Controllers::Calendars
+  class Create
+    include Web::Action
+
+    def call(params)
+    end
+  end
+end
